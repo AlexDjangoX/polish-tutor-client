e811d9b2d7b0dbc030eecf467da2ef615c488b5a
AlexDjangoX <alexmonk17@gmail.com>
2022-08-10 20:30:26 +0200

    Initial set-up, including fontaweseome

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path='/' element={<Navbar />} />
+        <Route path='/signup' element={<Navbar />} />
+        <Route path='/home' element={<Navbar />} />
+        <Route path='*' element={<Navigate replace to='/' />} />
+      </Routes>
+    </>
   );
 }
 
