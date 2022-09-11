1a31951fb137049645258089ca3946d67f364e6c
AlexDjangoX <alexmonk17@gmail.com>
2022-09-11 16:21:38 +0200

    Player is customised in TextToSpeech.js

--- src/components/textToSpeech/TextToSpeech.js
+++ src/components/textToSpeech/TextToSpeech.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
-import './TextToSpeech.css';
-import { useSpeechSynthesis } from 'react-speech-kit';
+import React, { useState } from "react";
+import "./TextToSpeech.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { useSpeechSynthesis } from "react-speech-kit";
 
 const TextToSpeech = ({ data, voices }) => {
   const [text, setText] = useState(data);
@@ -8,12 +10,11 @@ const TextToSpeech = ({ data, voices }) => {
   const [rate, setRate] = useState(1);
   const [voiceIndex, setVoiceIndex] = useState(21);
   const { speak, cancel, speaking, supported } = useSpeechSynthesis();
-
-  const voice = voices[voiceIndex] || '';
+  const voice = voices[voiceIndex] || "";
 
   return (
     <div>
-      <form className='text-to-speech-wrapper'>
+      <form className="text-to-speech-wrapper">
         {!supported && (
           <p>
             Oh no, it looks like your browser doesn&#39;t support Speech
@@ -22,71 +23,74 @@ const TextToSpeech = ({ data, voices }) => {
         )}
         {supported && (
           <>
-            <div className='select-options'>
-              <label htmlFor='voice'>Voice</label>
-              <select
-                id='voice'
-                name='voice'
-                value={voiceIndex}
-                onChange={(event) => {
-                  setVoiceIndex(event.target.value);
-                }}
-              >
-                <option value={7}>Paulina</option>
-                <option value={21}>Anna</option>
-                <option value={6}>Adam</option>
-              </select>
-            </div>
+            {/* <div className="select-options"> */}
+            <label htmlFor="voice">Voice</label>
+            <select
+              id="voice"
+              name="voice"
+              value={voiceIndex}
+              onChange={event => {
+                setVoiceIndex(event.target.value);
+              }}
+            >
+              <option value={7}>Paulina</option>
+              <option value={21}>Anna</option>
+              <option value={6}>Adam</option>
+            </select>
+            {/* </div> */}
 
-            <div className='select-rate'>
-              <div>
-                <label htmlFor='rate'>Rate: </label>
-                <div className='rate-value'>{rate}</div>
-              </div>
-              <input
-                type='range'
-                min='0.5'
-                max='2'
-                defaultValue='1'
-                step='0.1'
-                id='rate'
-                onChange={(event) => {
-                  setRate(event.target.value);
-                }}
-              />
-            </div>
-            <div className='select-pitch'>
-              <div>
-                <label htmlFor='pitch'>Pitch: </label>
-                <div className='pitch-value'>{pitch}</div>
-              </div>
-              <input
-                type='range'
-                min='0'
-                max='2'
-                defaultValue='1'
-                step='0.1'
-                id='pitch'
-                onChange={(event) => {
-                  setPitch(event.target.value);
-                }}
-              />
-            </div>
-
-            <div className='btn-speak'>
+            <div className="btn-speak">
               {speaking ? (
-                <button type='button' onClick={cancel}>
-                  Stop
+                <button className="play-btn" type="button" onClick={cancel}>
+                  <FontAwesomeIcon
+                    className="icon-placeholer"
+                    icon={regular("circle-pause")}
+                  />
                 </button>
               ) : (
                 <button
-                  type='button'
+                  type="button"
                   onClick={() => speak({ text, voice, rate, pitch })}
+                  className="play-btn"
                 >
-                  Speak
+                  <FontAwesomeIcon
+                    className="icon-placeholer"
+                    icon={regular("circle-play")}
+                  />
                 </button>
               )}
             </div>
+
+            {/* <div className="select-rate"> */}
+            <label htmlFor="rate">Rate: </label>
+            <span>{rate}</span>
+            <input
+              type="range"
+              min="0.5"
+              max="2"
+              defaultValue="1"
+              step="0.1"
+              id="rate"
+              onChange={event => {
+                setRate(event.target.value);
+              }}
+            />
+            {/* </div> */}
+            {/* <div className="select-pitch"> */}
+            <label htmlFor="pitch">Pitch: </label>
+            <span className="pitch-value">{pitch}</span>
+            <input
+              type="range"
+              min="0"
+              max="2"
+              defaultValue="1"
+              step="0.1"
+              id="pitch"
+              onChange={event => {
+                setPitch(event.target.value);
+              }}
+            />
+            {/* </div> */}
           </>
         )}
       </form>
