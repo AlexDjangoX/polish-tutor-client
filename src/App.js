d6eb319a5ff1dc0c5f0ba84e2e208f3308dc761a
AlexDjangoX <alexmonk17@gmail.com>
2022-11-22 13:18:05 +0100

    clean up

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
-import Timeline from './components/timeline/Timeline';
+
 import Kanban from './components/drag&drop/Kanban';
 import Profile from './components/profile/Profile';
 import Notes from './components/notes/Notes';
@@ -16,7 +16,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Hero />} />
-        <Route path='/experience' element={<Timeline />} />
         <Route
           path='/notatki'
           element={<Notes columns={columns} setColumns={setColumns} />}
@@ -26,7 +25,7 @@ function App() {
           element={<Kanban columns={columns} setColumns={setColumns} />}
         />
         <Route path='/profile' element={<Profile />} />
-        <Route path='*' element={<Timeline replace to='/' />} />
+        <Route path='*' element={<Hero replace to='/' />} />
       </Routes>
     </>
   );
