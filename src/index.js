342d925e072c6defec25fd55bb5c67b15279e930
AlexDjangoX <alexmonk17@gmail.com>
2022-12-10 10:05:11 +0100

    changed to HashRouter in index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN;
@@ -11,11 +11,11 @@ const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Router>
+  <HashRouter hashType='hash'>
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={`${window.location.origin}`}
+      redirectUri={`${window.location.href}`}
       audience='https://www.polish-api.com'
       scope='openid profile email'
       useRefreshTokens
@@ -23,5 +23,5 @@ root.render(
     >
       <App />
     </Auth0Provider>
-  </Router>
+  </HashRouter>
 );
